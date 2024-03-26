4a123eab7b0b3c00101a83ca7d0cb6ee2229903d
TeusCoder <mateusramalho883@gmail.com>
2024-03-25 22:30:36 -0300

    criacao dos paths para userEmpresa

--- src/routes/RotaEmpresas.ts
+++ src/routes/RotaEmpresas.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { CreateEmpresa } from "../controllers/crud/empresa";
+import { CreateEmpresa, deleteEmpresa, findAllEmpresas, findOneEmpresa, UpdateEmpresa } from "../controllers/crud/empresa";
 
 const route = Router();
 
-
 route.post("/", CreateEmpresa)
+route.put("/update", UpdateEmpresa)
+route.get("/findAll", findAllEmpresas)
+route.get("/findOne/:cnpj",findOneEmpresa)
+route.delete("/delete/:cnpj", deleteEmpresa)
+
 
 export default route
