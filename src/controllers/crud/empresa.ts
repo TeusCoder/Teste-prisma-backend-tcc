363c357696d91ab121b1a73e1366e3fec23db987
TeusCoder <mateusramalho883@gmail.com>
2024-04-02 20:16:44 -0300

    atualizado o arquivo de crud da empresa, todas as funções basicas etão funcionando

--- src/controllers/crud/empresa.ts
+++ src/controllers/crud/empresa.ts
@@ -1,85 +1,126 @@
+// id_userEmpresa  String @id @default(uuid())
+// id_endereco String @unique
+// endereco Endereco @relation(fields: [id_endereco], references: [id_endereco])
+// createdAt DateTime @default(now())
+// updatedAt DateTime @updatedAt
+// razaoSocial String @db.Char(100)
+// nome_fantasia String @db.Char(100)
+// cnpj String @unique @db.Char(14)
+// ie String @unique @db.Char(16)
+// telefone String @db.Char(11)
+// email String @unique @db.Char(100)
+// senha String
+// CriaVaga CriarVaga[]
+
 import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import { assert } from "console";
 
 const Empresa = new PrismaClient()
 
 async function CreateEmpresa(req: Request, res: Response) {
+    try {
+        //cria empresa
+        const {
+            id_endereco,
+            razaoSocial,
+            nome_fantasia,
+            cnpj,
+            ie,
+            telefone,
+            email,
+            senha } = req.body;
 
-    //cria empresa
-    const {
-        razaoSocial,
-        fantasia,
-        cnpj,
-        ie,
-        email,
-        senha } = req.body;
+        //criptografa a senha, usando a biblioteca bcrypt
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+        //verificar endereço
+        const VerificaEndereco = await Empresa.endereco.findUnique({
+            where: { id_endereco }
+        });
+        if (!VerificaEndereco) {
+            return res.status(404).json({ error: 'Endereço não encontrado.' });
+        };
 
-    //criptografa a senha, usando a biblioteca bcrypt
-    const senhaCriptografada = await bcrypt.hash(senha, 10);
+        const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
 
-    const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
-
-    // if (!usuarioExistente) {
-    //     const createdEmpresa = await Empresa.userEmpresa.create({
-    //         data: {
-    //             razaoSocial: razaoSocial,
-    //             fantasia: fantasia,
-    //             cnpj: cnpj,
-    //             ie: ie,
-    //             email: email,
-    //             senha: senhaCriptografada
-    //         }
-    //     })
-    //     res.status(201).json(createdEmpresa)
-
-    // } else {
-    //     res.status(400).send(`Usuario com esse cpf: ${cnpj} já existe!`)
-    // }
+        if (!usuarioExistente) {
+            const createdEmpresa = await Empresa.userEmpresa.create({
+                data: {
+                    id_endereco,
+                    razaoSocial,
+                    nome_fantasia,
+                    cnpj,
+                    ie,
+                    telefone,
+                    email,
+                    senha: senhaCriptografada
+                }
+            })
+            res.status(201).json(createdEmpresa);
+        } else {
+            res.status(400).send(`Usuario com esse cpf: ${cnpj} já existe!`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
-
+//atualizar empresa
+//passar no req.body e no data os atributos a serem mudados
 async function UpdateEmpresa(req: Request, res: Response) {
-    const { fantasia } = req.body;
-    // const EmpresaUpdated = await Empresa.userEmpresa.update(
-    //     {
-    //         where: {
-    //             email: 'superTintas@gmail.com'
-    //         },
-    //         data: { fantasia: fantasia }
-    //     })
-    // res.status(200).json(EmpresaUpdated)
+    try {
+        const { cnpj } = req.params;
+        const { nome_fantasia } = req.body;
+        const EmpresaUpdated = await Empresa.userEmpresa.update({
+            where: { cnpj },
+            data: { nome_fantasia }
+        });
+        res.status(200).json(EmpresaUpdated);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function findAllEmpresas(req: Request, res: Response) {
-    const empresas = await Empresa.userEmpresa.findMany();
-    res.status(200).json(empresas);
+    try {
+        const empresas = await Empresa.userEmpresa.findMany();
+        res.status(200).json(empresas);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function findOneEmpresa(req: Request, res: Response) {
-    const { cnpj } = req.params;
-    if (!cnpj) {
-        return res.status(404).end("Digite um cnpj válido!");
-    }
-    const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
-    if (!usuarioExistente) {
-        res.status(404).send(`Usuario com esse cpf: ${cnpj} não existe!`);
-    } else {
-        res.status(200).json(usuarioExistente)
+    try {
+        const { cnpj } = req.params;
+        if (!cnpj) {
+            return res.status(404).end("Digite um cnpj válido!");
+        }
+        const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
+        if (!usuarioExistente) {
+            res.status(404).send(`Usuario com esse cpf: ${cnpj} não existe!`);
+        } else {
+            res.status(200).json(usuarioExistente)
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
 async function deleteEmpresa(req: Request, res: Response) {
-    const { cnpj } = req.params;
-    if (!cnpj) {
-        return res.status(404).end("Digite um cnpj válido!");
-    }
-    const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
-    if (!usuarioExistente) {
-        res.status(404).send(`Usuario com esse cpf: ${cnpj} não existe!`);
-    } else {
-        const usuarioExistente = await Empresa.userEmpresa.delete({ where: { cnpj } });
-        res.status(200).end("usuario deletado");
+    try {
+        const { cnpj } = req.params;
+        if (!cnpj) {
+            return res.status(404).end("Digite um cnpj válido!");
+        }
+        const usuarioExistente = await Empresa.userEmpresa.findUnique({ where: { cnpj } });
+        if (!usuarioExistente) {
+            res.status(404).send(`Usuario com esse cpf: ${cnpj} não existe!`);
+        } else {
+            const usuarioExistente = await Empresa.userEmpresa.delete({ where: { cnpj } });
+            res.status(200).end("usuario deletado");
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
