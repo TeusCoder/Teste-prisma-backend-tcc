b110c01abce8d6dfbd49ffd6d606cf2b6ef4b113
TeusCoder <mateusramalho883@gmail.com>
2024-04-01 22:46:12 -0300

    alterado o arquivo das rotas do candidato para ser compativel com as ultimas atualizações

--- src/routes/RotaCandidato.ts
+++ src/routes/RotaCandidato.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
-import { createCandidato, deleteCandidato, exemplo, findAllCandidatos, findOneCandidato, isValid, UpdateCandidato } from "../controllers/crud/candidato";
+import { createCandidato, deleteCandidato,findAllCandidatos, findOneCandidato, isValid, UpdateCandidato } from "../controllers/crud/candidato";
 
 const route = Router();
 
-
 route.post("/", createCandidato)
 // route.post("/teste", isValid)
-route.put("/update", UpdateCandidato)
+route.put("/update/:email", UpdateCandidato)
 route.get("/findAll", findAllCandidatos)
 route.get("/findOne/:cpf",findOneCandidato)
 route.delete("/delete/:cpf", deleteCandidato)
-route.post("/teste", exemplo)
 
 export default route
