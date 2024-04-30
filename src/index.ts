3f7297d41168799665dfc283ba39d4609aaf7702
TeusCoder <mateusramalho883@gmail.com>
2024-04-30 19:12:27 -0300

    alterações para resolver os conflitos do posicionamento das rots

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,7 @@ import RouterCurriculo from './routes/RotaCurriculo'
 import RouterInscricoes from './routes/RotaInscricaoCandidatoVaga'
 import RouterCriaVaga from './routes/RotaCriaVaga'
 import RouterUploads from './routes/RotaUpload'
-<<<<<<< HEAD
 // import RouterLogin from './routes/RotaLogin'
-=======
-import RouterLogin from './routes/RotaLogin'
-import passport from "passport";
->>>>>>> 23f3ac75ba9e854894d82660f14b0cbff9d8ae8e
 import multer from "multer";
 import RouterUsers from './routes/RotaUser'
 
@@ -40,12 +35,7 @@ app.use("/vagas", RouterVaga)
 app.use("/curriculos", RouterCurriculo)
 app.use("/inscricoes", RouterInscricoes)
 app.use("/criarVagas", RouterCriaVaga)
-<<<<<<< HEAD
 // app.use("/login", RouterLogin)
-=======
-app.use("/login", RouterLogin)
-app.use('/token', passport.authenticate('jwt', { session: false }), RouterLogin);
->>>>>>> 23f3ac75ba9e854894d82660f14b0cbff9d8ae8e
 app.use("/uploads", express.static('uploads'), RouterUploads)
 app.use("/users", RouterUsers)
 
