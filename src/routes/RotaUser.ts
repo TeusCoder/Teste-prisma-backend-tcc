3665a10395c41366474a151c056e939d9cdc118d
TeusCoder <mateusramalho883@gmail.com>
2024-05-17 22:48:06 -0300

    corrigido erro textual ao chamar a rota de findOne

--- src/routes/RotaUser.ts
+++ src/routes/RotaUser.ts
@@ -6,7 +6,7 @@ const route = Router();
 route.post("/", createUser)
 route.put("/update/:id_user",updateUser)
 route.get("/findAll", findAllUsers)
-route.get("findOne/:id_user", findOneUser)
+route.get("/findOne/:id_user", findOneUser)
 route.delete("/delete/:id_user", deleteUser)
 
 export default route
