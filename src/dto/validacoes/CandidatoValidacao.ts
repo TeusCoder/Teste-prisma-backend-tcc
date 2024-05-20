9c2e253b6417fe7401288f458eb7e2f3466fe4b4
TeusCoder <mateusramalho883@gmail.com>
2024-05-20 19:41:59 -0300

    alterações na validação do zod, adicionado regex para validar telefone, cpf e cnpj

--- src/dto/validacoes/CandidatoValidacao.ts
+++ src/dto/validacoes/CandidatoValidacao.ts
@@ -5,7 +5,7 @@ export const candidatoSchema = z.object({
     id_endereco: z.string().uuid(),
     nome : z.string().min(3),
     sobrenome : z.string().min(3),
-    cpf : z.string().length(11),
+    cpf : z.string().length(11).regex(new RegExp('[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}')),
     dataNascimento : z.string(), //formato (YYYY-MM-DD)
-    telefone : z.string().length(11),
+    telefone : z.string().length(11).regex(new RegExp('^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[0-9])[0-9]{3}\-?[0-9]{4}$')),
 })
--- src/dto/validacoes/EmpresaValidacao.ts
+++ src/dto/validacoes/EmpresaValidacao.ts
@@ -5,7 +5,7 @@ export const EmpresaSchema = z.object({
     id_endereco : z.string().uuid(),
     razaoSocial: z.string().min(1),
     nome_fantasia : z.string().min(1),
-    cnpj : z.string().length(14),
+    cnpj : z.string().length(14).regex(new RegExp('[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2}')),
     ie: z.string().min(9),
     telefone : z.string().length(11),
 })
